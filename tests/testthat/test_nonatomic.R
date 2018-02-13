c2f1003edb7fcaf598560c4ebe8454a5ebd674a7
Martin Ueding <dev@martin-ueding.de>
2018-02-13 13:09:42 +0100

    Add a test for non-atomic types

---
+++ tests/testthat/test_nonatomic.R
@@ -0,0 +1,26 @@
+# A collections of tests that makes sure that non-trivial value types still work.
+
+context('Non-Atomic')
+
+p0 <- list(param = data.frame(a = c(3, 5),
+                              b = c('a', 'b')))
+
+test_that('list', {
+    # We have a function which returns a list with non-atomic vectors.
+    f <- function(param, value) {
+        list(v1 = list(3, 'X'),
+             v2 = p0)
+    }
+
+    v <- call.pvcontainer(f, p0)
+
+    # Now we need to make sure that we can easily access the variables within
+    # another function, i.e. the expression `value$v1` must work, we do not want
+    # to need `value[[1, 'v1']]` in order to get the variable.
+    f2 <- function(param, value) {
+        expect(!is.null(value$v1), 'value$v1 must not be NULL')
+        expect_equal(value$v1, list(3, 'X'))
+    }
+
+    v2 <- call.pvcontainer(f2, v)
+})

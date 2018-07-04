27ed852aac8388d298d92a25c6715e293701e988
Martin Ueding <dev@martin-ueding.de>
2018-07-04 13:47:02 +0200

    Remove .idx column after filtering

--- R/filter.R
+++ R/filter.R
@@ -5,7 +5,9 @@ filter_paramval <- function (pv, ...) {
     pv$param$.idx <- 1:nrow(pv$param)
 
     param_filtered <- dplyr::filter(pv$param, ...)
+    indices <- param_filtered$.idx
+    param_filtered$.idx <- NULL
 
     list(param = param_filtered,
-         value = pv$value[param_filtered$.idx])
+         value = pv$value[indices])
 }

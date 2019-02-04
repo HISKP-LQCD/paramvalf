d06e8e871fe972299cb038c133afc07201044ac6
Martin Ueding <dev@martin-ueding.de>
2019-02-04 09:12:30 +0100

    Add vignettes for now

---
+++ vignettes/framework-test.R
@@ -0,0 +1,25 @@
+# Some sample data in the format that we need.
+two <- list(param = data.frame(L = c(24),
+                               shift = c(TRUE, FALSE)),
+            value = data.frame(mass_two = c(3.0, 5.4)))
+
+four <- list(param = data.frame(L = c(24),
+                                vacuum = c(TRUE, FALSE)),
+             value = data.frame(mass_four = c(-33.1, 40),
+                                other_four = c('A', 'B')))
+
+# A function that generates a new data point.
+delta_E <- function(param, value) {
+    result = value$mass_two + value$mass_four
+
+    if (param$vacuum) {
+        result <- 2 * result
+    }
+
+    data.frame(delta_E = result,
+               other = 3.1)
+}
+
+
+res <- pvcall(delta_E, two, four)
+print(res)

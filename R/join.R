4c67294afaf1dc6b1fb274d4718fbf2c68f9fdd0
Martin Ueding <dev@martin-ueding.de>
2019-05-31 11:37:14 +0200

    Cope with grouped param tibbles

--- R/join.R
+++ R/join.R
@@ -13,8 +13,8 @@
 #' @export
 inner_outer_join_impl <- function(a, b) {
     # Extract the parameter sets from the containers.
-    ap <- tibble::as.tibble(a$param)
-    bp <- tibble::as.tibble(b$param)
+    ap <- dplyr::ungroup(tibble::as.tibble(a$param))
+    bp <- dplyr::ungroup(tibble::as.tibble(b$param))
 
     # The length of the value section (if present) must have exactly the same
     # number of elements as the parameter section.

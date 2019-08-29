d5b056d135264693e66b2473b112bad306c891e3
Martin Ueding <dev@martin-ueding.de>
2019-08-29 15:07:04 +0200

    Remove nonsense .idx parameter in ctor

--- R/class.R
+++ R/class.R
@@ -1,6 +1,5 @@
 paramval <- function (param, value) {
     pv <- list(param = param, value = value)
-    pv$param$.idx <- 1:nrow(pv$param)
     class(pv) <- append(class(pv), 'paramval')
     return (pv)
 }
